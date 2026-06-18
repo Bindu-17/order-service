51fa15444f889645fb04ad430cde14fed61af6f3
Bindu <bindu.jobsearch@gmail.com>
2026-06-18 19:43:00 +0530

    Created Order entity class

--- src/main/java/com/project/orderservice/entity/Order.java
+++ src/main/java/com/project/orderservice/entity/Order.java
@@ -1,4 +1,14 @@
 package com.project.orderservice.entity;
 
+import lombok.Data;
+
+@Data
+
 public class Order {
+    private Long orderId;
+    private Long customerId;
+    private String productName;
+    private Integer quantity;
+    private Double price;
+    private String status;
 }
